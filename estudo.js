7ad71a49a068b91043a77784985eaa6c77dfe697
Luana Vitória Nerilo Bilha De Souza <luana.bilha.souza@escola.pr.gov.br>
2022-10-25 15:41:00 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,3 +1,21 @@
-function total{
-let val= document.getElementById(valor).value
+.function total () {
+let val= document.getElementById("valor").value;
+let ju=document.getElementById("juros").value;
+
+let t= document.getElementById("juros").value;
+}
+if(Number)(val){
+    alert("o valor deve ser um número")
+    document.getElementById("valor").value= "";
+    document.getElementById("valor").focus();
+    return
+}
+let r= val;
+for (let m= l; m <= t; m++){
+    
+    
+
+}
+let resultado= (val * (1+ (ju/100)));
+Document.write("O resultado é" + resultado);
 }
